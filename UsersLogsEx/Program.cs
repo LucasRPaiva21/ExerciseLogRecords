cc57c281172dfdcad82b67b26c73528401db9727
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-06-20 23:19:34 -0300

    Versao final

--- UsersLogsEx/Program.cs
+++ UsersLogsEx/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UsersLogsEx.Entities;
 
 namespace UsersLogsEx
 {
@@ -6,7 +9,30 @@ namespace UsersLogsEx
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter the full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = Console.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instant});
+                    }
+                    Console.WriteLine("Total Users: " + set.Count);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
